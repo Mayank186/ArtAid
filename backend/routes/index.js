4e083b86772f626ed33f0e1d554152b79b40df76
Jayesh Prajapati <jayesh203.jp@gmail.com>
2021-11-25 18:21:50 -0500

    added route in main index.js file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,4 +7,7 @@ router.use('/auth', require('./auth'));
 // User Router
 router.use('/user', require('./users'));
 
+//Assignments 
+router.use('/assignments', require('./assignments'));
+
 module.exports = router;
